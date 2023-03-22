a87512f4b3d6fec88ad9090862bb0741429a3474
Bruno Pires <100307101+BrunoPires002@users.noreply.github.com>
2023-03-22 17:37:07 -0300

    Update AlunoController.cs

--- Escola/Controllers/AlunoController.cs
+++ Escola/Controllers/AlunoController.cs
@@ -18,13 +18,11 @@ namespace Escola.Controllers
             _context = context;
         }
 
-        // GET: Aluno
         public async Task<IActionResult> Index()
         {
             return View(await _context.Alunos.ToListAsync());
         }
 
-        // GET: Aluno/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -42,15 +40,11 @@ namespace Escola.Controllers
             return View(aluno);
         }
 
-        // GET: Aluno/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Aluno/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("IdAluno,NomeAluno,RgAluno,DataNascAluno")] Aluno aluno)
@@ -64,7 +58,6 @@ namespace Escola.Controllers
             return View(aluno);
         }
 
-        // GET: Aluno/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -80,9 +73,6 @@ namespace Escola.Controllers
             return View(aluno);
         }
 
-        // POST: Aluno/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("IdAluno,NomeAluno,RgAluno,DataNascAluno")] Aluno aluno)
@@ -115,7 +105,6 @@ namespace Escola.Controllers
             return View(aluno);
         }
 
-        // GET: Aluno/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -133,7 +122,6 @@ namespace Escola.Controllers
             return View(aluno);
         }
 
-        // POST: Aluno/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
